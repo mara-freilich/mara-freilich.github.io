8e7ed96601aa873fffd64e273daecf73715d0d76
Mara Freilich <mara.freilich@gmail..com>
2020-11-26 11:47:30 -0500

    toggle button

--- assets/js/partials/navbar.js
+++ assets/js/partials/navbar.js
@@ -21,3 +21,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
     mainStyle.backgroundPosition = '100% ' + parseInt(-x / 3) + 'px' + ', 0%, center top';
   });
 });
+
+hideShow.prototype.toggleRegion = function() {
+    
+    var thisObj = this;
+    
+    // toggle the region
+    this.$region.slideToggle(this.toggleSpeed, function() {
+                             
+                             if ($(this).attr('aria-expanded') == 'false') { // region is collapsed
+                             
+                             // update the aria-expanded attribute of the region
+                             $(this).attr('aria-expanded', 'true');
+                             
+                             // move focus to the region
+                             $(this).focus();
+                             
+                             // update the button label
+                             thisObj.$toggle.find('span').html('Hide');
+                             
+                             }
+                             else { // region is expanded
+                             
+                             // update the aria-expanded attribute of the region
+                             $(this).attr('aria-expanded', 'false');
+                             
+                             // update the button label
+                             thisObj.$toggle.find('span').html('Show');
+                             }
+                             });
+    
+} // end toggleRegion()
